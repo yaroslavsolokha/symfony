3741c95ad41d32958bf58f6cffca3a8737a7c6eb
Yaroslav Solokha <yaroslav.solokha@gmail.com>
2017-04-06 22:48:00 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+symfony
+=======
+
+A Symfony project created on March 23, 2017, 6:42 pm.
---
+++ web/test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yaroslav
+ * Date: 24/03/2017
+ * Time: 11:15
+ */
+
+phpinfo();
