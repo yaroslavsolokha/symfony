04c41a0fbd40c6401176b24fa45120d3005b6eb3
Yaroslav Solokha <yaroslav.solokha@gmail.com>
2017-04-23 01:43:39 +0200

    Add custom material layout

---
+++ web/assets/js/frontend.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yaroslav on 23/04/2017.
+ */
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
